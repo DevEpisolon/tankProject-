89166415240da55977cefcdbf9f5a5a375569612
Stephen Martinez <122754065+stephenm7777@users.noreply.github.com>
2023-10-25 22:03:00 -0700

    tank
    code for the project

---
+++ Assets/scripts/BulletHandler.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletHandler : MonoBehaviour
+{
+    public float launchSpeed = 75.0f;
+    public GameObject objectPrefab; 
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            SpawnObject(); 
+        }
+    }
+
+    void SpawnObject()
+    {
+        Vector3 spawnPosition = transform.position;
+        Quaternion spawnRotation = Quaternion.identity;
+
+        Vector3 localXDirection = transform.TransformDirection(Vector3.forward);
+        Vector3 velocity = localXDirection * launchSpeed; 
+        // Instantiate Object 
+        GameObject newObject = Instantiate(objectPrefab, spawnPosition, spawnRotation);
+
+        Rigidbody rb = newObject.GetComponent<Rigidbody>();
+        rb.velocity = velocity;
+    }
+}
